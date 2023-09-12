86ad71a67a71a0c1a4aaf295266d9988edfba9da
johannogbae <s223073945@deakin.edu.au>
2023-09-12 17:48:49 +1000

    task

---
+++ src/views/ConditionalRendering.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <h1 v-if="showTitle">Vue is awesome!</h1>
+      <h1 v-else>Is Vue awesome?</h1>
+      <button @click="toggleTitle">Toggle</button>
+      <p v-show="showParagraph">This paragraph is shown with v-show.</p>
+      <ul>
+        <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        showTitle: true,
+        showParagraph: true,
+        items: [
+          { id: 1, name: 'Item 1' },
+          { id: 2, name: 'Item 2' },
+          { id: 3, name: 'Item 3' },
+        ],
+      };
+    },
+    methods: {
+      toggleTitle() {
+        this.showTitle = !this.showTitle;
+      },
+    },
+  };
+  </script>
+  
---
+++ src/views/FormInputHandling.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+      <!-- Text Input -->
+      <input v-model="text" placeholder="Type something" />
+      <p>Text: {{ text }}</p> <br/><br/>
+  
+      <!-- Checkbox Input -->
+      <input type="checkbox" v-model="checked" />
+      <label>Checked: {{ checked }}</label> <br/><br/>
+  
+      <!-- Radio Input -->
+      <input type="radio" id="option1" value="Option 1 " v-model="selectedOption" />
+      <label for="option1">Option 1 </label>
+      <input type="radio" id="option2" value="Option 2 " v-model="selectedOption" />
+      <label for="option2">Option 2 </label>
+      <p>Selected Option: {{ selectedOption }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        text: '',
+        checked: false,
+        selectedOption: '',
+      };
+    },
+  };
+  </script>
+  
